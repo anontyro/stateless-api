3650f799b381beb8697130e322383254b911a912
Alexander Wilkinson <alexwilkinson@gmail.com>
2018-07-02 20:15:45 +0800

    adding in dependencies and then working on limiting return values from the blogs on none signed in users

--- services/auth/auth.js
+++ services/auth/auth.js
@@ -2,8 +2,6 @@
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 require('dotenv').config()
-const mongoose = require('mongoose');
-const util = require('../../utils/auth/auth');
 const connectToDatabase = require('../../connect');
 
 const User = require('../../models/auth/personSchema').Person;
--- services/blog/blog.js
+++ services/blog/blog.js
@@ -1,9 +1,31 @@
 'use strict'
+
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 require('dotenv').config()
 const mongoose = require('mongoose');
 const util = require('../../utils/auth/auth');
+const moment = require('moment');
 const connectToDatabase = require('../../connect');
 
 const Blog = require('../../models/blog/blogSchema').Blog;
+
+
+module.exports.getblogs = (callback) => {
+    const query = {
+        draft: false,
+        publish: moment()
+    };
+
+    connectToDatabase()
+        .then(() => {
+            Blog.find(query)
+                .then(blogList => callback({
+                    statusCode: 200,
+                    body: JSON.stringify({
+                        blogList: blogList
+                    })
+                }));
+        })
+        .catch(err => callback(err))
+}
