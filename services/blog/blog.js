506eff6383f309c664e0ab0f7dc8353dd0d81e05
Alexander Wilkinson <alexwilkinson@gmail.com>
2018-07-02 22:50:48 +0800

    added in the basic methods for the blog need to add in update still but the rest in place and need to check and test them

--- services/blog/blog.js
+++ services/blog/blog.js
@@ -4,7 +4,7 @@ var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 require('dotenv').config()
 const mongoose = require('mongoose');
-const util = require('../../utils/auth/auth');
+const util = require('../../utils/utils');
 const moment = require('moment');
 const connectToDatabase = require('../../connect');
 
@@ -13,8 +13,7 @@ const Blog = require('../../models/blog/blogSchema').Blog;
 
 module.exports.getblogs = (callback) => {
     const query = {
-        draft: false,
-        publish: moment()
+        draft: false
     };
 
     connectToDatabase()
@@ -28,4 +27,100 @@ module.exports.getblogs = (callback) => {
                 }));
         })
         .catch(err => callback(err))
+}
+
+module.exports.getBlog = (slug, callback) => {
+
+    const query = {
+        slug: slug
+    }
+
+    connectToDatabase()
+        .then(() => {
+            Blog.find(query)
+                .then(blog => callback({
+                    statusCode: 200,
+                    body: JSON.stringify({
+                        blog: blog
+                    })
+                }))
+        })
+        .catch(err => callback(err));
+}
+
+module.exports.createBlog = (event, callback) => {
+    const blog = JSON.parse(event.body);
+
+    if(!blog.title || !blog.body ) {
+        throw new Error('must contain a title and body');
+    }
+
+    if(blog.title.length < 5 || blog.body.length < 50) {
+        throw new Error('Title and bodt must have minimum content');
+    }
+
+    getUniqueSlug(title.replace(/\s/g, '_'), uniqueSlug => {
+        
+        blog.slug = uniqueSlug;
+        
+        connectToDatabase()
+            .then(() => {
+                Blog.create(blog)
+                    .then(response => {
+                        callback({
+                            statusCode: 201,
+                            body: JSON.stringify({
+                                blog: response,
+                                message: 'successfully create a new blog entry'
+                            })
+                        })
+                    })
+                    .catch(err => callback(err))
+            })
+    });
+}
+
+module.exports.deleteBlogById = (id, callback) => {
+    connectToDatabase()
+        .then(() => {
+            Blog.findByIdAndRemove(id)
+                .then(removed => {
+                    callback({
+                        statusCode: 200,
+                        body: JSON.stringify({
+                            blogId: id,
+                            message: 'successfully removed blog from the database',
+                            blog: removed
+                        })
+                    })
+                })
+                .catch(ex => callback(ex))
+        })
+}
+
+// remove the spaces from the title and make it a slug
+slugifyTitle = (title) => {
+    return new Promise( (resolve, reject) => {
+        resolve(title.replace(/\s/g, '_'));
+    })
+}
+
+// check the database for a unique value for the current slug
+getUniqueSlug = (slug, callback) => {
+    const rand = util(1, 99);
+    const nextSlug = slug + '_' + rand;
+
+    connectToDatabase()
+        .then(() => {
+            Blog.find({slug: nextSlug})
+            .then(results => {
+                if(!results) {
+                    callback(nextSlug);
+                } else {
+                    getUniqueSlug(slug, callback);
+                }
+            })
+        })
+        .catch(err => callback(err));
+
 }
--- services/blog/handler.js
+++ services/blog/handler.js
@@ -4,14 +4,76 @@ require('dotenv').config()
 const blog = require('../blog/blog');
 
 module.exports.getBlogs =(event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    try {
+        blog.getblogs(response => {
+            callback(null, response);
+        });
+    } catch(ex) {
+        console.err(ex);
+        callback(null, {
+            statusCode: ex.statusCode || 500,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'An error occured whilst trying to return the blog list'
+        });
+    }
 
 }
+
 module.exports.getBlog =(event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    const slug = event.pathParameters.slug;
+
+    try {
+        blog.getBlog(slug, response => {
+            callback(null, response);
+        })
+    } catch(ex) {
+        console.err(ex);
+        callback(null, {
+            statusCode: ex.statusCode || 500,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'An error occured whilst trying to return the blog list'
+        });
+    }
 
 }
+
 module.exports.createBlog =(event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
 
+    try {
+        blog.createBlog(event, response => {
+            callback(null, response);
+        })
+    } catch(ex) {
+        console.err(ex);
+        callback(null, {
+            statusCode: ex.statusCode || 500,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'An error occured whilst trying to return the blog list'
+        });
+    }
 }
-module.exports.deleteBlog =(event, context, callback) => {
 
+module.exports.deleteBlog =(event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    const id = event.pathParameters.id;
+
+
+    try {
+        blog.deleteBlogById(id, response => {
+            callback(null, response);
+        })
+    } catch(ex) {
+        console.err(ex);
+        callback(null, {
+            statusCode: ex.statusCode || 500,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'An error occured whilst trying to return the blog list'
+        });
+    }
 }
